c6e54e6b068b9085553dc7d7566918af996451a5
alaamigdady <alaamohammad283@gmail.com>
2019-11-29 21:48:12 +0200

    new classes

---
+++ src/Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+    private int nationalID;
+    private String firstName;
+    private String lastName;
+    private String nationality;
+    private String gender;
+    private String placeOfBirth;
+    private Date dateOfBirth;
+    private Department department;
+    private String userName;
+    private String passWord;
+    private String type;
+    private Salary salary;
+    private Attendance attendance;
+    private String email;
+
+    
+}
